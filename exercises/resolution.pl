ae760def8481f20c04755f74f1c3f8bff4c8e042
Smit Patel <patel.smit@northeastern.edu>
2023-11-21 21:23:29 -0500

    not classmate with self

--- exercises/resolution.pl
+++ exercises/resolution.pl
@@ -2,4 +2,4 @@ takes(jane_doe, his201).
 takes(jane_doe, cs254).
 takes(ajit_chandra, art302).
 takes(ajit_chandra, cs254).
-classmates(X, Y) :- takes(X, Z), takes(Y, Z).
+classmates(X, Y) :- takes(X, Z), takes(Y, Z), X \= Y.
