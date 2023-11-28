c63ae5cf7ce6b592d4fa4776161eb54749fe4d36
Smit Patel <patel.smit@northeastern.edu>
2023-11-28 17:24:32 -0500

    lab and list member exercises

--- exercises/labs.pl
+++ exercises/labs.pl
@@ -4,3 +4,12 @@ is_lab(shillman).
 takes(turing, cs307).
 takes(hopper, cs307).
 takes(alberuni, cs220).
+
+takes_lab(S) :- takes(S, Z), is_lab(W), meets_in(Z, W).
+
+% takes_lab(turing).
+% takes_lab(alberuni).
+% takes_lab(X).
+
+takes_lab2(S) :- takes(S, C), has_lab(C).
+has_lab(D) :- meets_in(D,R), is_lab(R).
--- exercises/member.pl
+++ exercises/member.pl
@@ -1,3 +1,6 @@
 /**
 * Define the rules to test if an element X is a member of a list
 */
+
+member(E, [E | _]).
+member(E, [_ | L]) :- member(E, L).
