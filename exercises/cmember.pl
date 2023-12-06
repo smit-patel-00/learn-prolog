9caf291b0875d42b63a29df757f610b3ae1a2dfa
Smit Patel <patel.smit@northeastern.edu>
2023-12-06 14:11:16 -0500

    cmember

--- exercises/cmember.pl
+++ exercises/cmember.pl
@@ -5,3 +5,21 @@
 
 member1(X, [X | _]) :- !.
 member1(X, [_ | T]) :- member1(X,T).
+
+cmember(X, L, N) :- X > N, !, member1(X, L).
+cmember(X, L, N) :- member1(N, L).
+
+% ?- cmember(4, [2,4,6,8], 3).
+% true.
+
+% ?- cmember(4, [2,4,6,8], 5).
+% false.
+
+% ?- cmember(4, [2,4,6,8], 6).
+% true.
+
+% ?- cmember(4, [2,4,6,8], 2).
+% true.
+
+% ?- cmember(4, [2,4,6,8], 7).
+% false.
